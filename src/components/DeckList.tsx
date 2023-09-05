6942d78cd13a246e358453ae9240e6e9bda5c55a
Simon Busard <simon.busard@gmail.com>
2023-09-05 21:50:43 +0200

    Improve visual when no deck is pasted

--- src/components/DeckList.tsx
+++ src/components/DeckList.tsx
@@ -3,10 +3,12 @@ import { Deck } from "../utils/parser";
 import { CardList } from "./CardList";
 
 export function DeckList(props: { deck: Deck }) {
-    return <div>
-        <h1 className="font-bold">Main deck</h1>
-        <CardList cards={props.deck.main} />
-        <h1 className="font-bold">Sideboard</h1>
-        <CardList cards={props.deck.sideboard} />
-    </div>;
+    if (props.deck.size === 0) return <div className="text-gray-500 italic">No deck</div>;
+    else
+        return <div>
+            <h1 className="font-bold">Main deck</h1>
+            <CardList cards={props.deck.main} />
+            <h1 className="font-bold">Sideboard</h1>
+            <CardList cards={props.deck.sideboard} />
+        </div>;
 }
