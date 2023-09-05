8d6a42ababd20aa1bccd9e1c5b9afd610f4cb919
Simon Busard <simon.busard@gmail.com>
2023-09-05 21:26:19 +0200

    Issue 1: fix parsing with duplicated cards

--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -1,13 +1,24 @@
+import _ from 'lodash';
+
 export interface Card {
     name: string;
     count: number;
 }
 
 export function parseDeck(deck: string): Card[] {
-    return deck.split('\n').flatMap((line) => {
+    const parsed = deck.split('\n').flatMap((line) => {
         const [countString, ...name] = line.split(' ');
         const count = parseInt(countString);
         if (Number.isNaN(count) || name.length === 0) return [];
         return {count: count, name: name.join(' ')};
     });
+    return compact(parsed);
+}
+
+function compact(deck: Card[]): Card[] {
+    const grouped = _.groupBy(deck, (card) => card.name);
+    return _.map(grouped, (cards, name) => ({
+        name,
+        count: _.sumBy(cards, (card) => card.count)
+    }))
 }
